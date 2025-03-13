ab768009eb1e0197703e839b1339f3a0f8779143
abdoulahmr <202034014503@univ-tebessa.dz>
2025-03-13 06:19:45 -1100

    15 commit in and still doesn't work

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./header";
+import Aside from "./aside";
+import Home from "./home";
+import Quote from "./quote";
+import Projects from "./projects";
+import Skills from "./skills";
+import AboutMe from "./about-me";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Aside />
+      <Home />
+      <Quote />
+      <Projects />
+      <Skills />
+      <AboutMe />
+    </>
+  );
+}
+
+export default App;
